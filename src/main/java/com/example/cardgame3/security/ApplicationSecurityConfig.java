a6b7e28207c0dcd3567cf00bfb14652b1dfd2538
motigon95 <44246326+motigon95@users.noreply.github.com>
2022-07-29 16:55:04 +0200

    Security: Basic user roles and permissions defined

--- src/main/java/com/example/cardgame3/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/cardgame3/security/ApplicationSecurityConfig.java
@@ -1,13 +1,26 @@
 package com.example.cardgame3.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +32,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails motiUser = User.builder()
+                .username("moti")
+                .password(passwordEncoder.encode("password"))
+                .roles(ApplicationUserRole.PLAYER.name())
+                .build();
+
+       UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(motiUser, adminUser);
     }
 }
---
+++ src/main/java/com/example/cardgame3/security/ApplicationUserPermission.java
@@ -0,0 +1,17 @@
+package com.example.cardgame3.security;
+
+public enum ApplicationUserPermission {
+
+    PLAYER_SAVECARD("player:savecard"),
+    PLAYER_GETPLAYERS("player:getplayers");
+
+    private final String permission;
+
+    ApplicationUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
