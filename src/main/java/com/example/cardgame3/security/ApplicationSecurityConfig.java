146c5888e4bc36471e5afb05dc75db51b72f774d
motigon95 <44246326+motigon95@users.noreply.github.com>
2022-07-29 15:53:09 +0200

    Spring security basic configurations (Whitelisted endpoints)

---
+++ src/main/java/com/example/cardgame3/security/ApplicationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.cardgame3.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/api/cards")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
